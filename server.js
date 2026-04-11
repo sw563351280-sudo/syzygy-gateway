fc1806ed2bca177d8ef3327893b721c8dfb175be
sw563351280-sudo <sw563351280@gmail.com>
2026-04-11 23:30:17 +0900

    Update server.js

--- server.js
+++ server.js
@@ -2142,6 +2142,8 @@ if (name === "read_webpage") {
                             }
                         } catch(e3) {}
                     }
+                    }
+                }
                  } else if (act.type === 'type' && act.selector) {
                 await page.waitForSelector(act.selector, { timeout: 5000 }).catch(function(){});
                 await page.type(act.selector, act.value || '');
